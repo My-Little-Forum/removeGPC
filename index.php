98e6e1efdf87dd2f84ba66e9dbba8e4987fc0539
Heiko August <post@auge8472.de>
2017-02-09 13:14:25 +0100

    Add: index file with package informations

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Script to correct with PHP magic quoted text in a MySQL database.
+ *
+ * @package removeGPC
+ * @author H. August <post@auge8472.de>
+ * @copy 2017
+ * @version 0
+ * @license https://opensource.org/licenses/GPL-2.0  GNU Public License version 2
+ */
+
+?>
