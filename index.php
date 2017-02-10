9fd10e0efd41b7807a2c8e378de6bb214ad44cdb
Heiko August <auge8472@users.noreply.github.com>
2017-02-10 14:00:24 +0100

    first bits of code for structure (#2)
    * Add: index file with package informations
    * Add: database functions and their inclusion in the main script
    * Add: new files script.ini and .htaccess and parsing code of ini file
    * Change: connecting the database server
    * Change: define first part for the general structure of the script

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Script to correct with PHP magic quoted text in a MySQL database.
+ *
+ * @package removeGPC
+ * @author H. August <post@auge8472.de>
+ * @copy 2017
+ * @version 0
+ * @license https://opensource.org/licenses/GPL-2.0  GNU Public License version 2
+ */
+
+require_once "data/scripts/funcs.db.php";
+
+$errors = array();
+$settings = parse_ini_file("data/config/script.ini", TRUE);
+
+$cid = dBase_Connect($settings['db']);
+
+if (is_array($cid) and $cid[0] === false) {
+	$errors[] = $cid[1];
+}
+if (empty($errors) and (!is_array($settings['tables']) or empty($settings['tables']))) {
+}
+
+if (empty($errors)) {
+}
+
+?>
